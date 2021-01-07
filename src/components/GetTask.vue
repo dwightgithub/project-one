6e2e9ee0e8230e8e275a333cfd67cbcb9ab93581
Dwight Zhang <dwight12@qq.com>
2021-01-07 20:05:55 +0800

    提交修改

--- src/components/GetTask.vue
+++ src/components/GetTask.vue
@@ -1,40 +1,27 @@
 <template>
-  <div id="getTask" style="width: 600px;">
-    <el-form ref="form" :model="task" label-width="80px">
-      <el-form-item label="任务类型">
-        <el-switch
-          style="display: block"
-          v-model="task.taskType"
-          active-color="#13ce66"
-          inactive-color="#ff4949"
-          active-text="对比"
-          inactive-text="评分"
-        ></el-switch>
-      </el-form-item>
-      <el-form-item label="评价维度">
-        <el-radio-group v-model="task.resource">
-          <el-radio label="美观"></el-radio>
-          <el-radio label="安全"></el-radio>
-          <el-radio label="生态"></el-radio>
-          <el-radio label="贫穷"></el-radio>
-          <el-radio label="繁忙"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="任务组数">
-        <el-slider v-model="task.groupCount" show-stops :min="5" :max="20" :marks="marks"></el-slider>
-      </el-form-item>
-      <el-form-item label="任务区域">
-        <el-cascader
-          placeholder="试试搜索：成都市"
-          filterable
-          v-model="task.region"
-          :options="options"
-          :props="{  multiple: true }"
-        ></el-cascader>
-      </el-form-item>
-    </el-form>
-    <el-button style="margin-left: 10;" @click="CreateTask">创建任务</el-button>
-  </div>
+	<div id="getTask" style="width: 600px;">
+		<el-form ref="form" :model="task" label-width="80px">
+			<el-form-item label="任务类型">
+				<el-switch style="display: block" v-model="task.taskType" active-color="#13ce66" inactive-color="#ff4949"
+				 active-text="对比" inactive-text="评分">
+				</el-switch>
+			</el-form-item>
+			<el-form-item label="评价维度">
+				<el-radio-group v-model="task.resource">
+					<el-radio label="美观"></el-radio>
+					<el-radio label="安全"></el-radio>
+					<el-radio label="天气"></el-radio>
+				</el-radio-group>
+			</el-form-item>
+			<el-form-item label="任务组数">
+				<el-slider v-model="task.groupCount" show-stops :min="5" :max="20" :marks="marks"> </el-slider>
+			</el-form-item>
+			<el-form-item label="任务区域">
+				<el-cascader placeholder="试试搜索：成都市" filterable v-model="task.region" :options="options" :props="{  multiple: true }"></el-cascader>
+			</el-form-item>
+		</el-form>
+		<el-button style="margin-left: 10;" @click="CreateTask">创建任务</el-button>
+	</div>
 </template>
 
 <script>
@@ -69,7 +56,7 @@ export default {
       ],
       task: {
         taskType: true, //0是评分任务，1是对比任务
-        resource: "美观", //0是美观，1是安全，2是生态，3是贫穷，4是繁忙
+        resource: "美观",//0是美观，1是安全，2是生态，3是贫穷，4是繁忙
         groupCount: 10,
         region: []
       }
@@ -98,11 +85,10 @@ export default {
           .post("https://localhost:5001/api/login/createTask", obj)
           .then(res => {
             if (res) {
-              //创建任务成功后，设置将新任务信息放到VUEX里，由StreetViewMain主界面控制具体任务消隐，任务界面出现后会加载内容
-              this.$store.commit("setTaskInfo", {
-                taskID: res.data.taskID,
-                taskType: obj.taskType
-              });
+              //创建任务成功后，设置任务号，关闭创建页面，打开任务界面
+              this.$store.commit("setTaskID", res.taskID);
+              this.$emit("parChangeTabShow", 4 - obj.taskType, true);
+              this.$emit("parChangeTabShow", "2", false);
             }
           })
           .catch(e => {
@@ -114,12 +100,12 @@ export default {
 </script>
 
 <style>
-#getTask {
-  background: url("../assets/moroccan-flower.png") repeat;
-  background-position: center;
+	#getTask {
+		background: url("../assets/moroccan-flower.png") repeat;
+		background-position: center;
 
-  height: 100%;
-  width: 100%;
-  position: fixed;
-}
+		height: 100%;
+		width: 100%;
+		position: fixed;
+	}
 </style>
--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="UploadImage" style="width: 600px;">
-      <el-upload class="upload-demo" drag action="" :limit="10" multiple>
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖动至此处,或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg文件，不超过10M</div>
-      </el-upload>
+  <div id="UploadImage" style="width: 600px">
+    <el-upload class="upload-demo" drag action="" limit="10" multiple>
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖动至此处,或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip">只能上传jpg文件，不超过10M</div>
+    </el-upload>
   </div>
-
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
 };
 </script>
 
